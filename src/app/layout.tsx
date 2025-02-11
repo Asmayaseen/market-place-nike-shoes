33ba2d37af9349f62565f8f14dbc9da322469837
Asma yaseen <159251930+Asmayaseen@users.noreply.github.com>
2025-02-11 17:29:11 +0500

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { CartProvider } from "@/components/CartContext";
 import { WishListProvider } from "@/components/WishList";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/nextjs";
 import { ToastContainer } from "react-toastify";
-import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Nike Store",
@@ -25,33 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-   
-      <html lang="en">
-        <head>
-          <link
-            rel="shortcut icon"
-            href="/assets/Nike.png"
-            type="image/x-icon"
-          />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-        </head>
-        <body>
-          <CartProvider>
-            <WishListProvider>
-             
-              <Header />
-
-              {children}
-              
-              <ToastContainer />
-              <Footer />
-            </WishListProvider>
-          </CartProvider>
-        </body>
-      </html>
-    
+    <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/assets/Nike.png" type="image/x-icon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <CartProvider>
+          <WishListProvider>
+            <Header />
+            {children}
+            <ToastContainer />
+            <Footer />
+          </WishListProvider>
+        </CartProvider>
+      </body>
+    </html>
   );
 }
